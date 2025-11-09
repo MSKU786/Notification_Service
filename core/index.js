8a72e087294081bb2968df6b5621f71b2e6e472d
MSKU786 <manishsingh332000@gmail.com>
2025-11-09 11:54:02 +0530

    Initailzed the project

---
+++ core/index.js
@@ -0,0 +1,20 @@
+const Koa = require('koa');
+const app = new Koa();
+const PORT = process.env.PORT || 3000;
+const context = {db: {}, logger: console};
+
+app.use(async (ctx, next) => {
+  ctx.db = context.db;
+  ctx.logger = context.logger;
+  await next();
+});
+
+app.use(async (ctx, next) => {
+  ctx.logger.log('Request received');
+  await next();
+});
+
+(async () => {
+  await loadPlugins();
+  app.listen(PORT, () => console.log('Server running on port 3000'));
+})();
